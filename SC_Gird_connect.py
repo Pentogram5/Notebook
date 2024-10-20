050ebb8df4d976b4a8abeb291c965258918f604a
plokiju <plokiju@niuitmo.ru>
2024-10-20 22:13:22 +0500

    feat: end all functions

--- SC_Gird_connect.py
+++ SC_Gird_connect.py
@@ -1,20 +1,8 @@
-'''
-Дано
-- self.robot_color
-- self.base_color
-- get_objects
-- _get_our_raw_position
-Нужно
-- путь для захвата ближайшего кубика (не включая позицию кубика)
-— блокируй те объекты, что заняты чужим роботом
-- путь до корзины
-'''
+from SC_Gird import *
+# from SC_detectors import * - надо добавить метод получения данных с YOLO
 
-# from Test import *
-# from SC_Gird import *
-# from SC_detectors import *
-
-def calculate_vertex_objects(x, y, base_map):
+def calculate_vertex_objects(x, y, g): # Вычисление вершины в которой находится объект
+    base_map = g.return_node_coordinate()
     for bound in base_map:
         length = bound[0]
         wight = bound[1]
@@ -25,61 +13,72 @@ def calculate_vertex_objects(x, y, base_map):
         if x_r <= length and y_r <= wight:
             return bound[3]
 
-def build_center(x1, y1, x2, y2):
+def build_center(x1, y1, x2, y2): # Вычисление центра координат объекта
 
     x = (x1+x2)/2
     y = (y1+y2)/2
 
     return x, y
 
-def get_nearest_cube_path(data, base_map):
+def get_nearest_cube_path(data, robot_coordinates): # Поиск минимального пути для кубов
     paths = []
+    size  = 5
+    g = Graph(size)
+    g.set_standart_map()
+    base_map = g.return_node_coordinate()
+    add_data(g, data)
     for key, value in data.items():
         if key == 'cube':
             for index, data in enumerate(value):
-                x, y = build_center(data[0], data[1], data[2], data[3])
-                vertex = calculate_vertex_objects(x, y, base_map)
-
-                paths.append(get_current_path(data, vertex))
-
-    return paths
-
-def add_data(g, data):
-    base_map = g.return_node_coordinate()
-    for key, value in data.items():
-        for index, data in enumerate(value):
-            x, y = build_center(data[0], data[1], data[2], data[3])
-            vertex = calculate_vertex_objects(x, y, base_map)
-            some_obj = [vertex, key, data[0]-data[2], data[1]-data[3], x, y]
-            print(some_obj)
-            g.add_object_to_node(some_obj)
-
-def get_current_path(g, aim):
-
+                try:
+                    x, y = build_center(data[0], data[1], data[2], data[3])
+                    vertex = calculate_vertex_objects(x, y, g)
+                    paths.append(get_current_path(g, vertex, robot_coordinates))
+                except Exception as e:
+                    continue
+    min_path_count = 100
+    min_path = 0
+    for p in range(0, len(paths)):
+        if len(paths[p]) <= min_path_count:
+            min_path_count = len(paths[p])
+            min_path = paths[p]
     
-    # start_vertex = g.get_node_our_robot(250.0, 300.0)
-    # # start_vertex = (0,0)
-    # start = start_vertex
-    # goal = aim
+    print(min_path)
+    return min_path
 
-    g.remove_edge_by_objects()
-
-    path = g.find_shortest_path(start, goal)
-    g.visualize(path)
-    del path[-1]
-
-    print(path)
-    return path
-
-def create_base(data):
+def get_to_base(data, robot_coordinates, base_coordinates): # Поиск минимального пути до базы
     size  = 5
     g = Graph(size)
     g.set_standart_map()
     add_data(g, data)
-    
 
+    return get_current_path(g, base_coordinates, robot_coordinates)
+
+def add_data(g, data): # Добавление данных в граф 
+    for key, value in data.items():
+        for index, data in enumerate(value):
+            x, y = build_center(data[0], data[1], data[2], data[3])
+            vertex = calculate_vertex_objects(x, y, g)
+            some_obj = [vertex, key, data[0]-data[2], data[1]-data[3], x, y]
+            print(some_obj)
+            g.add_object_to_node(some_obj)
+
+def get_current_path(g, aim, robot_coordinates): # Построение графа
+    x, y = build_center(robot_coordinates[0],robot_coordinates[1],robot_coordinates[2],robot_coordinates[3])
+    start_vertex = g.get_node_our_robot(x, y)
+    start = start_vertex
+    g.remove_edge_by_objects()
+    path = g.find_shortest_path(start, aim)
+    g.visualize(path)
+    del path[-1]
+    print(path)
+    return path
+
+
+# Example
 dict = {
     "cube": [[100, 150, 200, 250],[50, 10, 10, 50]],
-    "robot": [[300, 350, 200, 250]]}
+    "robot": [[300, 350, 200, 250]],
+    "alien": [[30, 35, 20, 20]]}
 
-get_nearest_cube_path(dict)
+get_nearest_cube_path(dict, [300, 350, 200, 250])
