582aa84ad80a72b31cde651825edb3327c057d89
Fe0dor MacBook Air M1 <woolpig@yandex.ru>
2024-10-20 21:12:37 +0500

    work but must be tested

--- SC_frontCamera.py
+++ SC_frontCamera.py
@@ -14,7 +14,6 @@ class WebCamera:
         self.th = None
         # ЗДЕСЬ ИНИЦИАЛИЗИРУЙ КАМЕРУ
         self.cam = cv2.VideoCapture('http://192.168.2.12:8080/?action=stream')
-        self.model = YOLO('yolo_for_camera_robot.pt')
         self.tr = ThreadRate(max_fps)
     
     def start_capturing_image(self):
@@ -27,61 +26,22 @@ class WebCamera:
         if not ret:
             return False, None
         frame = cv2.rotate(frame, cv2.ROTATE_180)
-        self.res = self.model(frame)
         return frame, self.res
 
     def show_image(self, frame, res):
-        if not res:
-            return
-        for result in res:
-            boxes = result.boxes  # Get bounding box outputs
-            for box in boxes:
-                # Extract coordinates and class information
-                x1, y1, x2, y2 = map(int, box.xyxy.flatten().cpu().numpy())  # Convert to integers
-                score = box.conf.item()  # Confidence score
-                cls = result.names[box.cls.int().item()]  # Class name
-
-                # Draw the bounding box and label on the image
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)  # Green rectangle
-                cv2.putText(frame, f'{cls} {score:.2f}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        print('===================')
         cv2.imshow('video feed', frame)
         #cv2.imshow('gray feed', gray)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             return
     
-
-        
     def _start_capturing_image(self):
         while True:
             self.frame, self.res = self.read_image()
-            self.show_image(self.frame, self.res)
-
+            # self.show_image(self.frame, self.res)
             self.tr.sleep()
     
     def read(self):
         return self.frame is not None, self.frame
-    
-    def center(self, obj_class):
-        xc = 0
-        yc = 0
-        max_conf = 0
-        fl = False
-        for result in self.res:
-            boxes = result.boxes
-            for box in boxes:
-                cls = result.names[box.cls.int().item()]
-                score = box.conf.item()
-                if cls == obj_class and score > max_conf:
-                    x1, y1, x2, y2 = map(int, box.xyxy.flatten().cpu().numpy())  # Convert to integers
-                    xc = (x1 + x2) // 2
-                    yc = (y1 + y2) // 2
-                    max_conf = score
-                    fl = True
-        if fl:
-            return xc, yc, max_conf
-        else:
-            return None, None, max_conf
     
     @staticmethod
     def VideoCapture(*args):
@@ -89,3 +49,4 @@ class WebCamera:
         obj.start_capturing_image()
         return obj
 
+cap = WebCamera.VideoCapture()
