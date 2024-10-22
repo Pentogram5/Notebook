6d8f3aa2fa4c77668de7510ad6b94212d1c50eb6
Fe0dor MacBook Air M1 <woolpig@yandex.ru>
2024-10-22 14:40:00 +0500

    99

--- test_fedya.py
+++ test_fedya.py
@@ -4,10 +4,10 @@ from SC_API_tcp import *
 from SC_advenced_movement import ram
 from SC_INS import *
 from advanced_camera.SC_CS import show_sm_point
-from advanced_camera.SC_get_direction import *
+from SC_Gird_connect import *
 
 
-tch = TopCameraHandler(0, framework=CamFrameWorks.testVideo, fake_img_update_period=2, use_undist=True,
+tch = TopCameraHandler(0, framework=CamFrameWorks.cv2, fake_img_update_period=2, use_undist=True,
                        robot_color=RobotColors.RED)
 # # while True:
 # #     # print(tch.results, tch.timestamp)
@@ -62,15 +62,25 @@ ins.start_updater()
 
 #     # Устанавливаем скорости моторов
 #     ram.set_speeds(v, w)
-robots = [
-    Robot('green', True, []),
-    Robot('red', False, [])
-]
+
+def getBoxCenter(xyxy):
+        return [xyxy[0] + (xyxy[2] - xyxy[0]) / 2, xyxy[1] + (xyxy[3] - xyxy[1]) / 2]
+    
+def lenght(A, B):
+    print(A, B)
+    return ((A[0] - B[0]) ** 2 + (A[1] - B[1])**2) ** 0.5
+
+def getBoxCenter(self, xyxy):
+    return [xyxy[0] + (xyxy[2] - xyxy[0]) / 2, xyxy[1] + (xyxy[3] - xyxy[1]) / 2]
+
+def lenght(self, A, B):
+    print(A, B)
+    return ((A[0] - B[0]) ** 2 + (A[1] - B[1])**2) ** 0.5
 
 print('NIGGER')
 while True:
-    
-    ret, frame = tch.read_yolo()
+        
+    ret, frame_in = tch.read_yolo()
     # print('NIGGERS', frame)
     if not ret:
         continue
@@ -86,46 +96,46 @@ while True:
     # print(phase4, tch.is_yolo_running, tch.timestamp_yolo)
     
     results, timestamp = tch.get_results()
-    # if results is not None:
-    #     frame, vec = get_img_and_res(frame_in.copy(), results)
-    #     print(vec)
+    if results is not None:
+        frame, vec = get_img_and_res(frame_in.copy(), results)
+        # print(vec)
     
     # ram.w = 10
     
     # update_speeds()
     # ram.set_speeds(20, 20)
-    print(tch.get_our_raw_position())
+    # res = tch.get_our_raw_position()
+    # print(res)
+    data = tch.get_objects()
+    # print(data)
+    res_sm = get_our_robot_pos_4_sm(frame, results, 'green') #fix
+    print("res_sm: ", res_sm)
+    res = get_our_robot_pos_4(frame, results, 'green') #fix
+    res22 = get_direction(frame, results)
+    barriers = find_barriers(frame, results)  
+    min_path_to_cube = get_closest_PL(data, res_sm, barriers)
 
-    res = get_our_robot_pos_4(frame, results, 'red')
-    if res is not None:
-        x1, y1, x2, y2 = res
-        p = ((x1 + x2) // 2, (y1 + y2) // 2)
-        cv2.circle(frame, list(map(int,p)), 5, (0, 255, 255), 5)
 
-    if results != None:
-        for result in results:
-            boxes = result.boxes  # Get bounding box outputs
-            for box in boxes:
-                # Extract coordinates and class information
-                x1, y1, x2, y2 = map(int, box.xyxy.flatten().cpu().numpy())  # Convert to integers
-                score = box.conf.item()  # Confidence score
-                cls = result.names[box.cls.int().item()]  # Class name
 
-                # Draw the bounding box and label on the image
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)  # Green rectangle
-                cv2.putText(frame, f'{cls} {score:.2f}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-        res = get_direction(frame, results)
-
-        for item in res:
-            line,  xx, yy = item
-            x1, y1, x2, y2 = line
-            cv2.line(frame, (xx + x1, yy + y1), (xx + x2, yy + y2), (0, 0, 255), thickness=4)
-            cv2.circle(frame, (xx + x2, yy + y2), radius=10, color=(0, 0, 255))
+    # for cube
+    if min_path_to_cube != 0:
+        for i in range(1, len(min_path_to_cube)):
+            frame = show_sm_line(frame, get_koeffs(results), min_path_to_cube[i - 1], min_path_to_cube[i])
     
-    # points = [(i*10, 45) for i in range(10)]
-    # for p in points:
-    #     show_sm_point(frame, tch.koefs, p)
+    
+    for result in results:
+        boxes = result.boxes  # Get bounding box outputs
+        for box in boxes:
+            # Extract coordinates and class information
+            x1, y1, x2, y2 = map(int, box.xyxy.flatten().cpu().numpy())  # Convert to integers
+            score = box.conf.item()  # Confidence score
+            cls = result.names[box.cls.int().item()]  # Class name
+
+            # Draw the bounding box and label on the image
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)  # Green rectangle
+            cv2.putText(frame, f'{cls} {score:.2f}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    
+
     robots = []
     accs = []
     averageLenghtCubeToCenter = []
@@ -143,11 +153,40 @@ while True:
                 print(getBoxCenter(xyxy), list(frame.shape)[:-1])
                 averageLenghtCubeToCenter.append(lenght(getBoxCenter(xyxy), list(frame.shape)[:-1]))
     print('==============', averageLenghtCubeToCenter)
+
+
+
+
+    for item in res22:
+        line,  xx, yy = item
+        x1, y1, x2, y2 = line
+        cv2.line(frame, (xx + x1, yy + y1), (xx + x2, yy + y2), (0, 0, 255), thickness=4)
+        cv2.circle(frame, (xx + x2, yy + y2), radius=10, color=(0, 0, 255))
+    
+    if res is not None:
+        x1, y1, x2, y2 = res
+        p = ((x1 + x2) // 2, (y1 + y2) // 2)
+        cv2.circle(frame, list(map(int,p)), 5, (0, 255, 255), 5)
+    res, _ = tch.get_our_raw_position()
+    if res is not None:
+        p = res
+        show_sm_point(frame, tch.koefs, p, color=(0,0,255))
+    print(tch.koefs)
+    show_sm_point(frame, tch.koefs, (100, 45))
+    show_sm_point(frame, tch.koefs, (100+55, 45))
+    show_sm_point(frame, tch.koefs, (100, 45+70*3))
+    show_sm_point(frame, tch.koefs, (0,0))
+    show_sm_point(frame, tch.koefs, (100+55+70+55+15, 45))
+    
+    show_sm_point(frame, tch.koefs, (100+55+70+55+15, 45+70*3))
+    show_sm_point(frame, tch.koefs, (400, 310))
+    
+    points = [(i*10, 45) for i in range(10)]
+    for p in points:
+        show_sm_point(frame, tch.koefs, p)
     cv2.imshow('Video Stream', frame)
     # #cv2.imshow('Video Stream', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    # time.sleep(1)
-    time.sleep(0.03)
-
-    
+    time.sleep(2)
+    # time.sleep(0.03)
